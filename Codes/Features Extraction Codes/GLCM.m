48cf2a7c12a73a9248f3a9f54960595600b2636a
Krishanu Dev Sarma <43542359+KrishanuDevSarma@users.noreply.github.com>
2024-02-02 16:05:05 +0530

    Create GLCM.m

---
+++ Codes/Features Extraction Codes/GLCM.m
@@ -0,0 +1,53 @@
+%%Extracting GLCM Features from the Images 
+for Idx = 1:152
+       
+        glcms = graycomatrix(adhist);
+        R=0;
+        for i = 1:8
+            for j = 1:8
+                R = R + glcms(i,j);
+            end;
+        end;
+       
+        normalized_glcms = glcms./R;
+        homogeneity = 0;
+        contrast = 0;
+        energy = 0; correlation = 0;
+        mean_glcms = 0;
+        var_glcms = 0;
+        for i = 1:8
+            for j = 1 : 8
+
+%Detecting the value of Homogeneity feature
+                homogeneity = homogeneity + (normalized_glcms(i,j)/(1 + (i-j)^2));
+
+%Detecting the value of Contrast feature
+                contrast = contrast + (normalized_glcms(i,j)*(i-j)^2);
+
+%Detecting the value of Energy feature
+                energy = energy + normalized_glcms(i,j)^2;
+                mean_glcms = mean_glcms + i*normalized_glcms(i,j);
+ 
+            end
+        end
+ 
+        for i = 1:8
+            for j = 1:8
+                var_glcms = var_glcms + (normalized_glcms(i,j)*(i-mean_glcms)^2);
+            end
+        end
+ 
+        for i = 1:8
+            for j = 1:8
+
+%Detecting the value of Correlation feature
+                correlation = correlation + ((normalized_glcms(i,j)*(i-mean_glcms)*(j - mean_glcms))/var_glcms);
+            end
+        end
+        feature(:,Idx) = [homogeneity;correlation;contrast;energy]
+end
+
+%Storing the combined GLCM Features of all the Images as a single matrix 
+GLCM_feature = feature.';
+
+horzcat = [Features GLCM_feature];
